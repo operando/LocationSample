460d773b293d5a06baed8346899914519f29f7d7
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2015-07-21 00:05:31 +0900

    Get Location

--- LocationSample/ViewController.swift
+++ LocationSample/ViewController.swift
@@ -7,19 +7,68 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        println(UIDevice.currentDevice().systemName)
+        println(UIDevice.currentDevice().systemVersion)
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func getLocation(sender: AnyObject) {
+        let status = CLLocationManager.authorizationStatus()
+        switch status {
+        case .Denied:
+            NSLog(".Denied")
+            break
+        case .Restricted:
+            NSLog(".Restricted")
+            break
+        case .NotDetermined:
+            if locationManager.respondsToSelector("requestWhenInUseAuthorization") {
+//                locationManager.requestWhenInUseAuthorization()
+                locationManager.requestAlwaysAuthorization()
+            } else {
+                locationManager.startUpdatingLocation()
+            }
+        case .AuthorizedWhenInUse, .AuthorizedAlways:
+            locationManager.startUpdatingLocation()
+        default:
+            break
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        switch status{
+        case .Restricted, .Denied:
+            manager.stopUpdatingLocation()
+        case .AuthorizedAlways, .AuthorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+        default:
+            break
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        if locations.count > 0 {
+            let currentLocation = locations.last as? CLLocation
+            NSLog("緯度:\(currentLocation?.coordinate.latitude) 経度:\(currentLocation?.coordinate.longitude)")
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
+        println(error.description)
+    }
 }
 
